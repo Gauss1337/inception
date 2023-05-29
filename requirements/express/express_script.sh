9229aa31e501779aac7ebb71a215ac9c02e45b39
Gauss1337 <ayoub.hakam90@gmail.com>
2023-05-29 04:06:38 +0100

    makefile needed

--- requirements/express/express_script.sh
+++ requirements/express/express_script.sh
@@ -1,6 +1,8 @@
+sed -i "s/_ENV_DB_PASSWORD/$_WORDPRESS_DB_PASSWORD/g" wp-config.php
+sed -i "s/_ENV_DB_NAME/$_WORDPRESS_DB_NAME/g" wp-config.php
+sed -i "s/_ENV_DB_HOST/$_WORDPRESS_DB_HOST/g" wp-config.php
+sed -i "s/_ENV_DB_USER/$_WORDPRESS_DB_USER/g" wp-config.php
 cd /var/www/html/wordpress
-sed -i "s/_ENV_DB_PASSWORD/$_WORDPRESS_DB_PASSWORD/g" wp-conf.php
-sed -i "s/_ENV_DB_NAME/$_WORDPRESS_DB_NAME/g" wp-conf.php
-sed -i "s/_ENV_DB_HOST/$_WORDPRESS_DB_HOST/g" wp-conf.php
-sed -i "s/_ENV_DB_USER/$_WORDPRESS_DB_USER/g" wp-conf.php
+wp core install --url=your_website_url --title="9irsh" --admin_user=$_WORDPRESS_DB_USER --admin_password=$_WORDPRESS_DB_PASSWORD --admin_email=ahakam@student.1337.ma --allow-root
+
 php-fpm7.3 -F -R
