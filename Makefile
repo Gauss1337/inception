e4ac4ccda424a72e7a11275e2c1ca7f8e9ebdca7
Gauss1337 <ayoub.hakam90@gmail.com>
2023-05-31 04:02:37 +0100

    done

--- Makefile
+++ Makefile
@@ -6,14 +6,14 @@ build:
 	$(DOCKER_COMPOSE) -f $(DOCKER_COMPOSE_FILE) build 
 
 start:
-	$(DOCKER_COMPOSE) -f $(DOCKER_COMPOSE_FILE) up -d
+	$(DOCKER_COMPOSE) -f $(DOCKER_COMPOSE_FILE) up -d 
 
 stop:
 	$(DOCKER_COMPOSE) -f $(DOCKER_COMPOSE_FILE) down
 
 clean:
 	$(DOCKER_COMPOSE) -f $(DOCKER_COMPOSE_FILE) down -v
-	rm -rf /Users/ahakam/data/mariadb/*
-	rm -rf /Users/ahakam/data/wordpress/*
+	sudo rm -rf /home/ahakam/data/mariadb/*
+	sudo rm -rf /home/ahakam/data/wordpress/*
 	docker system prune --force --all
 
--- srsc/requirements/express/tools/express_script.sh
+++ srsc/requirements/express/tools/express_script.sh
@@ -4,6 +4,6 @@ sed -i "s/_ENV_DB_HOST/$_WORDPRESS_DB_HOST/g" wp-config.php
 sed -i "s/_ENV_DB_USER/$_WORDPRESS_DB_USER/g" wp-config.php
 cd /var/www/html/wordpress
 wp core install --url=ahakam.42.fr --title="9irsh" --admin_user=$_WORDPRESS_DB_USER --admin_password=$_WORDPRESS_DB_PASSWORD --admin_email=$_WORDPRESS_EMAIL --allow-root
-wp user create $_WP_CLI_USER $_WP_CLI_EMAIL --user_pass=$_WP_CLI_PASSWORD
+wp user create $_WP_CLI_USER $_WP_CLI_EMAIL --user_pass=$_WP_CLI_PASSWORD --allow-root
 php-fpm7.3 -F -R
 
